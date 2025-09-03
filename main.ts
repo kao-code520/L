1c1c04511f10e66f3777362e33c13964a9e4b4fb
kao-code520 <kao058@ms1.ttps.kh.edu.tw>
2025-09-04 05:05:49 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("I")
+    basic.showIcon(IconNames.Heart)
+    basic.showString("U")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("I LOVE U")
+})
+basic.showIcon(IconNames.Yes)
